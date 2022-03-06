df78a9e31a2c972feb4cefaaf7ee1018a4de22eb
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-06 19:40:55 +0300

    dn

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,9 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        
+        HStack{
+        Image("dog1")
+            .resizable()
+            .scaledToFit()
+            .frame(width:100, height:100, alignment: .center)
+            .clipShape(Circle())
+            
+            Text("woofy.official")
+            
+            Spacer()
+
+        }
+        
+        HStack{
+            
+        Image("dog2")
+            .resizable()
+            .scaledToFit()
+            .frame(width:150, height:150, alignment: .center)
+
+        Image("dog3")
+            .resizable()
+            .scaledToFit()
+            .frame(width:150, height:150, alignment: .center)
+        
+        Image("dog4")
+            .resizable()
+            .scaledToFit()
+            .frame(width:150, height:160, alignment: .center)
+        }
+        
+        HStack{
+        Image("dog5")
+            .resizable()
+            .scaledToFit()
+            .frame(width:170, height:200, alignment: .center)
+        
+        Image("dog6")
+            .resizable()
+            .scaledToFit()
+            .frame(width:150, height:200, alignment: .center)
+        
+        Image("dog7")
+            .resizable()
+            .scaledToFit()
+            .frame(width:150, height:200, alignment: .center)
             .padding()
+            
+
     }
+        Spacer()
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
